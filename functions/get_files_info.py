031217ec7c255e78dcdbe45a7e65bc838c80656a
KriKri98 <1000lpkrikri@gmail.com>
2026-02-17 10:16:53 +0100

    Added Check Filepath

---
+++ functions/get_files_info.py
@@ -0,0 +1,25 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    working_dir_abs = os.path.abspath(working_directory)
+    target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    try:
+        if valid_target_dir == False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+        items_in_directory = []
+        for items in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, items)
+            size = os.path.getsize(item_path)
+            is_dir = os.path.isdir(item_path)
+            items_in_directory.append(f"- {item_path}: file_size={size}, is_dir={is_dir}")
+        return "\n".join(items_in_directory)
+    except Exception as e:
+        return f"Error: {e}"
+
+
+    
+
+    
