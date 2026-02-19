0b12c064fc7b8c97c6098004c82fa7925ec48d6d
KriKri98 <1000lpkrikri@gmail.com>
2026-02-19 15:42:16 +0100

    Added feedback loop

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from call_function import available_functions, call_function
 import argparse
 from google.genai import types
 from google import genai
+import sys
 parser = argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
@@ -22,30 +23,36 @@ messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)]
 
 def main():
   
-
-    response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0, tools=[available_functions]))
-    if response.usage_metadata == None:
-        raise RuntimeError("failed API request")
-    if args.verbose == True:
-        print(f"User prompt: {messages}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    response_list = []
-    if response.function_calls != None:
-        for function_call in response.function_calls:
-            function_call_result = call_function(function_call, args.verbose)
-            if function_call_result.parts == []:
-                raise Exception("Error: function parts are empty")
-            if function_call_result.parts[0].function_response == None:
-                raise Exception("Error: function response of first function part is None")
-            if function_call_result.parts[0].function_response.response == None:
-                raise Exception("Error: response of function response is None")
-            response_list.append(function_call_result.parts[0])
-            if args.verbose == True:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-            
-    else:
-        print(response.text)
+    for i in range(20):
+        response = client.models.generate_content(model="gemini-2.5-flash", contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0, tools=[available_functions]))
+        if response.usage_metadata == None:
+            raise RuntimeError("failed API request")
+        if response.candidates != None:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+        if args.verbose == True:
+            print(f"User prompt: {messages}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        response_list = []
+        if response.function_calls != None:
+            for function_call in response.function_calls:
+                function_call_result = call_function(function_call, args.verbose)
+                if function_call_result.parts == []:
+                    raise Exception("Error: function parts are empty")
+                if function_call_result.parts[0].function_response == None:
+                    raise Exception("Error: function response of first function part is None")
+                if function_call_result.parts[0].function_response.response == None:
+                    raise Exception("Error: response of function response is None")
+                response_list.append(function_call_result.parts[0])
+                messages.append(types.Content(role="user", parts=response_list))
+                if args.verbose == True:
+                    print(f"-> {function_call_result.parts[0].function_response.response}")          
+        else:
+            print(response.text)
+            return
+    print("Maximum model calls reached")
+    sys.exit(1)
     
 
 if __name__ == "__main__":
